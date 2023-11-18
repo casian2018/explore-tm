00eb9b7c878998558bc5cfd39075f52ea508b97a
CasianTheProgrammator <99408730+casian2018@users.noreply.github.com>
2023-11-18 20:51:31 +0200

    push

--- components/rent.vue
+++ components/rent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container mx-auto p-4">
+    <div class="container mx-auto p-4 w-1/2 text-center">
       <h1 class="text-3xl font-bold mb-4">Rent a Bike or Scooter</h1>
   
       <!-- Step 1: Choose Your Ride -->
